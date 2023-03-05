3cee4713e6e06204ccfea1358b7900b5c9d32201
alexandrosporfiris <alexandrosporfiris@hotmail.gr>
2023-03-05 22:32:33 +0200

    dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'midnight-express';
+export class AppComponent implements OnInit {
+  private _jsonURL = '../assets/movies.json';
+  public movies: any  = [];
+
+  constructor(private http: HttpClient) {
+    this.getJSON().subscribe((data) => {
+      this.movies = data;
+    });
+  }
+  public getJSON(): Observable<any> {
+    return this.http.get(this._jsonURL);
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
